9298418fb62d52b4b3120ab2676f8c96b3be46e1
Volodymyr Ivashchenko <vladimir.v.iv@gmail.com>
2025-08-31 09:00:39 +0200

    the Bun Logo is removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {APITester} from "./APITester";
 import "./index.css";
 
-import logo from "./logo.svg";
 import reactLogo from "./react.svg";
 import {MarketDashboard} from "./MarketDashboard";
 
@@ -9,7 +8,6 @@ export function App() {
   return (
     <div className="app">
       <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo"/>
         <img src={reactLogo} alt="React Logo" className="logo react-logo"/>
       </div>
 
